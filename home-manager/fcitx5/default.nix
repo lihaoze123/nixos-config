6a48eb2eae27ac3fcefc44b3fccde1580fd0d911
chumeng <2595248810@qq.com>
2026-07-06 17:11:11 +0800

    Update input method and development tooling

--- home-manager/fcitx5/default.nix
+++ home-manager/fcitx5/default.nix
@@ -1,4 +1,27 @@
 { pkgs, ... }:
+let
+  rime-shuangpin-fuzhuma-version = "1.0.8";
+  rime-shuangpin-fuzhuma-src = pkgs.fetchzip {
+    url = "https://github.com/gaboolic/rime-shuangpin-fuzhuma/releases/download/${rime-shuangpin-fuzhuma-version}/rime-moqi-schemas.zip";
+    hash = "sha256-WNXLexWBdgqMsHxw8Cp1yZB85HxZ4VmfX5f86sfCfHY=";
+    stripRoot = false;
+  };
+
+  rime-shuangpin-fuzhuma = pkgs.stdenvNoCC.mkDerivation {
+    pname = "rime-shuangpin-fuzhuma";
+    version = rime-shuangpin-fuzhuma-version;
+    dontUnpack = true;
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/share/rime-data
+      cp -r ${rime-shuangpin-fuzhuma-src}/. $out/share/rime-data/
+      cp ${pkgs.rime-data}/share/rime-data/stroke.* $out/share/rime-data/
+
+      runHook postInstall
+    '';
+  };
+in
 {
   xdg.configFile = {
     "fcitx5/profile" = {
@@ -15,7 +38,7 @@
     fcitx5.addons = with pkgs; [
       (fcitx5-rime.override {
         rimeDataPkgs = [
-          pkgs.rime-ice
+          rime-shuangpin-fuzhuma
         ];
       })
       qt6Packages.fcitx5-configtool
--- home-manager/shell/common.nix
+++ home-manager/shell/common.nix
@@ -26,5 +26,6 @@
     grim
     slurp
     tree-sitter
+    ghostty
   ];
 }
